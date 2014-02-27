70dc9ee871b5c1467bba488348dceaad215382e6
keelan <pkarmstr@gmail.com>
2014-02-27 12:54:53 -0500

    init feature testing suite

---
+++ feature_function_tester.py
@@ -0,0 +1,15 @@
+__author__ = 'keelan'
+
+import unittest
+from feature_functions import *
+
+class FeatureTest(unittest.TestCase):
+
+    def setUp(self):
+        """data goes here"""
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
+
