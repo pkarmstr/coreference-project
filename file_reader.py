e3f8421392c6366a57408ad4f48b15ea72487911
keelan <pkarmstr@gmail.com>
2014-03-01 17:50:00 -0500

    fixed

--- file_reader.py
+++ file_reader.py
@@ -7,10 +7,13 @@ from nltk.corpus import BracketParseCorpusReader
 def tree_reader():
     d = {}
     trees = BracketParseCorpusReader("parsed_sentences/", ".*")
-    for name in tree_reader.fileids():
+    for name in trees.fileids():
         d_name = re.sub(r"\.tree", "", name)
         d[d_name] = list(trees.parsed_sents(name))
 
     return d
 
 TREES_DICTIONARY = tree_reader()
+
+if __name__ == "__main__":
+    print TREES_DICTIONARY["NYT20001230.1309.0093.head.coref.raw"]
