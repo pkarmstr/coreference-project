dfcb65d3064875ebcbce6b989076ad1198286644
Keelan Armstrong <pkarmstr@tiara.cs.brandeis.edu>
2014-03-02 14:32:20 -0500

    everything works, might run into merging issues

--- batch_parse_charniak.sh
+++ batch_parse_charniak.sh
@@ -5,11 +5,9 @@ RAW=raw_sentences/
 PARSER="/home/j/clp/chinese/bin/charniak-parse.sh"
 TREES=parsed_sentences/
 
-python26 build_raw_sentences.py $POS $RAW
-
 for f in $RAW/*; do \
 	x=${f%.raw}
-	$PARSER $f > $TREES${f##*/}.tree 
+	$PARSER $f > $TREES${f##*/}.tree &
 done
 
 echo "DONE"
