b811e41fb525fbfcf08a8307143265aa453470a1
Julia Bosque <jbosgil@googlemail.com>
2014-03-06 22:17:00 -0500

    added test_max_NP

--- feature_function_tester.py
+++ feature_function_tester.py
@@ -144,7 +144,7 @@ class FeatureTest(unittest.TestCase):
     def test_same_max_NP(self):
         line1 = "NYT20001111.1247.0093.head.coref 24 3 4 PER photographer 24 5 7 PER Yola_Monakhov photographer Yola_Monakhov yes".rstrip().split()
         feats1 = FeatureRow(*line1)
-        print same_max_NP(feats1)
+        self.assertEqual(same_max_NP(feats1).endswith("True"),True)
 
 
 
