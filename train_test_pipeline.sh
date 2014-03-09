6ebde50fcf729c6a57259fed2d12b652ad55c432
Keelan Armstrong <pkarmstr@tiara.cs.brandeis.edu>
2014-03-09 03:53:34 -0400

    new experiment

--- train_test_pipeline.sh
+++ train_test_pipeline.sh
@@ -2,30 +2,31 @@
 EXPERIMENT_DIR="$1"
 TYPE="$2"
 PYTHON="/home/g/grad/pkarmstr/python27/bin/python2.7"
+KEELAN="/home/g/grad/pkarmstr/infoextraction/coreference-project"
 
 # build all of the feature vectors
 
-$PYTHON feature_generator.py resources/coref-trainset.gold \
+$PYTHON $KEELAN/feature_generator.py resources/coref-trainset.gold \
 	$EXPERIMENT_DIR/train.gold $EXPERIMENT_DIR/feature_list.txt -a
 
-$PYTHON feature_generator.py resources/coref-"$TYPE"set.gold \
+$PYTHON $KEELAN/feature_generator.py resources/coref-"$TYPE"set.gold \
 	$EXPERIMENT_DIR/$TYPE.gold $EXPERIMENT_DIR/feature_list.txt -a
 
-$PYTHON feature_generator.py resources/coref-"$TYPE"set.notag \
+$PYTHON $KEELAN/feature_generator.py resources/coref-"$TYPE"set.notag \
 	$EXPERIMENT_DIR/$TYPE.notag $EXPERIMENT_DIR/feature_list.txt
 
 # training
-./mallet-maxent-classifier.sh -train \
+$KEELAN/mallet-maxent-classifier.sh -train \
 	-model=$EXPERIMENT_DIR/model \
 	-gold=$EXPERIMENT_DIR/train.gold
 
 # testing - doesn't work yet?
-./mallet-maxent-classifier.sh -classify  \
+$KEELAN/mallet-maxent-classifier.sh -classify  \
 	-model=$EXPERIMENT_DIR/model \
 	-input=$EXPERIMENT_DIR/$TYPE.notag > $EXPERIMENT_DIR/$TYPE.tagged
 
 # evaluation
-python coref-evaluator.py $EXPERIMENT_DIR/$TYPE.gold \
+python $KEELAN/coref-evaluator.py $EXPERIMENT_DIR/$TYPE.gold \
 	$EXPERIMENT_DIR/$TYPE.tagged > $EXPERIMENT_DIR/"$TYPE"_eval.txt
 
 echo Finished everything, results at $EXPERIMENT_DIR/"$TYPE"_eval.txt
